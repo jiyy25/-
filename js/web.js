f7825e1b81f7a0266d936fa3df018fd6e28071d6
jiyy25 <jiyy825@gmail.com>
2023-10-30 15:47:26 +0900

    제이쿼리 선택자연습!

--- js/web.js
+++ js/web.js
@@ -1,47 +1,35 @@
-$(document).ready(function () { 
-    //화면이 준비되면 실행해
+// $(document).ready(function () {
+//     $("h1 a").html("다지워")
+
+//     $("#gnb > li").eq(1).find(".d1a").html("네이버")
+
+//     //대메뉴의 소메뉴 중 첫번째 텍스트가 구글
+//     $(".d2ul").eq(2).find(".d2a").eq(0).html("구글");
+//     $("#gnb >li:eq(2) li:eq(0) a").html("구글");
+
+//     // $("body").html("")
+
+//     //모든 링크를 안되게 처리
+//     $("a").attr("href", "#none")
+
+//     $("h1 img").attr("src", "logo.jpg")
+//     //attr(속성, 값)->속성에 접근
+
+//     //toggleXClass, addClass, removeClass
+//     $("h1 img").addClass("on")
+
+//     //css(스타일, 값)
+//     $("body").css("background", "yellow")
+
+//     //자바스크립을 이용해 스타일이 들어감->동적객체
+// })
+
+
+$(document).ready(function () {
+    //gif인 이미지 없애줘
+    $('img[src$=".gif"]').css("display","none");
+
+    //링크값이 없는 a태그 없애줘
+    $('a[href="]').css("display", "none");
+
 })
-
-
-// #gnb를 클릭하면 실행하시오.
-$("#gnb").click(function () {
-
-})
-
-$("#gnb li:nth-child(1)").click(function () {
-
-});
-
-$("#gnb li").eq(0).click(function () {
-    //첫번째child
-});
-$("#gnb li").eq(-1).click(function () {
-    //라스트child
-});
-
-//링크가 걸린 이미지를 클릭 실행 하시오.
-$("a img").click(function () {
-
-});
-
-$("button#cart").click(function () {
-
-});
-
-$("#gnb li").find("a").click(function () {
-
-});
-
-$("#gnb li").eq(0).find(a).click(function () {
-
-});
-
-//제이쿼리 이벤트의 종류
-//ready ->document
-//click ->전태그
-//hover ->전태그
-//change -> checkbox, radio, input, select
-//select-> change로 거의 다쓰기 때문에 잘 안씀
-//submit ->button,input
-//focus ->input[type=text]
-
