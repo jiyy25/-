ca010c4a08c25bcaf7cd5c9f7c9741ef9b8e6ff3
jiyy25 <jiyy825@gmail.com>
2023-10-30 14:32:37 +0900

    리스트 한번에 호버하기

---
+++ js/web.js
@@ -0,0 +1,47 @@
+$(document).ready(function () { 
+    //화면이 준비되면 실행해
+})
+
+
+// #gnb를 클릭하면 실행하시오.
+$("#gnb").click(function () {
+
+})
+
+$("#gnb li:nth-child(1)").click(function () {
+
+});
+
+$("#gnb li").eq(0).click(function () {
+    //첫번째child
+});
+$("#gnb li").eq(-1).click(function () {
+    //라스트child
+});
+
+//링크가 걸린 이미지를 클릭 실행 하시오.
+$("a img").click(function () {
+
+});
+
+$("button#cart").click(function () {
+
+});
+
+$("#gnb li").find("a").click(function () {
+
+});
+
+$("#gnb li").eq(0).find(a).click(function () {
+
+});
+
+//제이쿼리 이벤트의 종류
+//ready ->document
+//click ->전태그
+//hover ->전태그
+//change -> checkbox, radio, input, select
+//select-> change로 거의 다쓰기 때문에 잘 안씀
+//submit ->button,input
+//focus ->input[type=text]
+
